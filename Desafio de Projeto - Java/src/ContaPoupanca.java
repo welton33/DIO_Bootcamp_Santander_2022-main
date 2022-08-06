4ad8965df16a09f881cb7f063dda04f9a3ba6bc4
welton33 <39927905+welton33@users.noreply.github.com>
2022-08-06 16:11:29 -0300

    Add files via upload

---
+++ Desafio de Projeto - Java/src/ContaPoupanca.java
@@ -0,0 +1,14 @@
+
+public class ContaPoupanca extends Conta {
+
+	public ContaPoupanca(Cliente cliente) {
+		super(cliente);
+	}
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("*** Conta Poupança *** ");
+		super.imprimirInfosComuns();
+	}
+
+}
